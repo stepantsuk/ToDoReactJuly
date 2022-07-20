1da47699113dd2b0e2150f3c51dc73b3b6ee16ae
stepantsuk <stepan.tsukanov@yandex.ru>
2022-07-20 11:35:16 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 //import ToDoWrapperClasses from "./classes/ToDoWrapperClasses";
-import { Wrapper } from "./withHooks/Wrapper/index";
+import { Wrapper } from "./withHooks/Wrapper";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
